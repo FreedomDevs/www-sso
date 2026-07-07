c28f86783aa3c3c4b6d40f85db4947a71b2b8862
Foksif <knenrerp@gmail.com>
2026-07-07 17:16:19 +0700

    Переделал Input чтобы он мог принимать ref

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,47 +1,57 @@
 import clsx from 'clsx';
+import { forwardRef } from 'react';
+
 import styles from './Input.module.css';
 import { InputProps } from './Input.types';
 
-export function Input({
-  label,
-  error,
-  leftIcon,
-  rightIcon,
-  className,
-  variant = 'default',
-  id,
-  ...props
-}: InputProps) {
-  return (
-    <div className={styles.wrapper}>
-      <div
-        className={clsx(
-          styles.container,
-          styles[variant],
-          error && styles.error
-        )}
-      >
-        {leftIcon && <span className={styles.icon}>{leftIcon}</span>}
-
-        <div className={styles.inputWrapper}>
-          <input
-            id={id}
-            className={clsx(styles.input, className)}
-            placeholder=" "
-            {...props}
-          />
-
-          {label && (
-            <label htmlFor={id} className={styles.label}>
-              {label}
-            </label>
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      label,
+      error,
+      leftIcon,
+      rightIcon,
+      className,
+      variant = 'default',
+      id,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <div className={styles.wrapper}>
+        <div
+          className={clsx(
+            styles.container,
+            styles[variant],
+            error && styles.error
           )}
+        >
+          {leftIcon && <span className={styles.icon}>{leftIcon}</span>}
+
+          <div className={styles.inputWrapper}>
+            <input
+              ref={ref}
+              id={id}
+              className={clsx(styles.input, className)}
+              placeholder=" "
+              {...props}
+            />
+
+            {label && (
+              <label htmlFor={id} className={styles.label}>
+                {label}
+              </label>
+            )}
+          </div>
+
+          {rightIcon && <span className={styles.icon}>{rightIcon}</span>}
         </div>
 
-        {rightIcon && <span className={styles.icon}>{rightIcon}</span>}
+        {error && <span className={styles.errorText}>{error}</span>}
       </div>
+    );
+  }
+);
 
-      {error && <span className={styles.errorText}>{error}</span>}
-    </div>
-  );
-}
+Input.displayName = 'Input';
