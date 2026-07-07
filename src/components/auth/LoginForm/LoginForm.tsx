9cb837632436b6ee4118700776da88c77a8fc16b
Foksif <knenrerp@gmail.com>
2026-07-07 17:17:10 +0700

    Валидация форм

--- src/components/auth/LoginForm/LoginForm.tsx
+++ src/components/auth/LoginForm/LoginForm.tsx
@@ -1,31 +1,65 @@
 'use client';
 
+import { useForm } from 'react-hook-form';
+import Link from 'next/link';
+
 import { Input } from '@/src/components/ui/Input';
 import { Button } from '@/src/components/ui/Button';
-import Link from 'next/link';
 
 import styles from './LoginForm.module.css';
 import { FaGithub, FaGoogle } from 'react-icons/fa';
 
+type LoginFormValues = {
+  email: string;
+  password: string;
+};
+
 export function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormValues>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (data: LoginFormValues) => {
+    console.log(data);
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <Input
         id="email"
         placeholder="Email или имя пользователя"
-        variant={'default'}
-        label={"Email"}
+        variant="default"
+        label="Email или Login"
+        error={errors.email?.message}
+        {...register('email', {
+          required: 'Введите email',
+        })}
       />
 
       <Input
         id="password"
         placeholder="Пароль"
-        type={'password'}
-        variant={'default'}
-        label={"Password"}
+        type="password"
+        variant="default"
+        label="Password"
+        error={errors.password?.message}
+        {...register('password', {
+          required: 'Введите пароль',
+          minLength: {
+            value: 6,
+            message: 'Минимум 6 символов',
+          },
+        })}
       />
 
-      <Button fullWidth={true} variant={'primary'} size={'sm'}>
+      <Button type="submit" fullWidth variant="primary" size="sm">
         Продолжить
       </Button>
 
@@ -37,19 +71,21 @@ export function LoginForm() {
 
       <div className={styles.socials}>
         <Button
-          size={'sm'}
+          size="sm"
           variant="integrations"
           leftIcon={<FaGoogle />}
           fullWidth
+          type="button"
         >
           Google
         </Button>
 
         <Button
-          size={'sm'}
+          size="sm"
           variant="integrations"
           leftIcon={<FaGithub />}
           fullWidth
+          type="button"
         >
           GitHub
         </Button>
@@ -57,11 +93,13 @@ export function LoginForm() {
 
       <div className={styles.links}>
         <Link href="/passwordless">Войти с Passkey</Link>
+
         <Link href="/forgot-password">Забыли пароль?</Link>
       </div>
 
       <div className={styles.register}>
         <span>Нет аккаунта?</span>
+
         <Link href="/auth/register">Зарегистрироваться</Link>
       </div>
     </form>
--- src/components/auth/RegisterForm/RegisterForm.tsx
+++ src/components/auth/RegisterForm/RegisterForm.tsx
@@ -1,38 +1,94 @@
 'use client';
 
-import { Input } from '@/src/components/ui/Input';
-import { Button } from '@/src/components/ui/Button';
+import { useForm } from 'react-hook-form';
 import Link from 'next/link';
-
-import styles from './RegisterForm.module.css';
 import { FaGithub, FaGoogle } from 'react-icons/fa';
 
+import { Input } from '@/src/components/ui/Input';
+import { Button } from '@/src/components/ui/Button';
+
+import styles from './RegisterForm.module.css';
+
+type RegisterFormValues = {
+  email: string;
+  login: string;
+  password: string;
+};
+
 export function RegisterForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormValues>({
+    defaultValues: {
+      email: '',
+      login: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (data: RegisterFormValues) => {
+    console.log(data);
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <Input
         id="email"
         placeholder="Email"
-        variant={'default'}
-        label={'email'}
+        variant="default"
+        label="Email"
+        error={errors.email?.message}
+        {...register('email', {
+          required: 'Введите email',
+          pattern: {
+            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+            message: 'Введите корректный email',
+          },
+        })}
       />
 
       <Input
         id="login"
         placeholder="Логин"
-        variant={'default'}
-        label={'Логин'}
+        variant="default"
+        label="Логин"
+        error={errors.login?.message}
+        {...register('login', {
+          required: 'Введите логин',
+          minLength: {
+            value: 3,
+            message: 'Логин минимум 3 символа',
+          },
+          maxLength: {
+            value: 16,
+            message: 'Логин максимум 16 символов',
+          },
+          pattern: {
+            value: /^[a-zA-Z_0-9]+$/,
+            message: 'Только латинские буквы, цифры и символ _',
+          },
+        })}
       />
 
       <Input
         id="password"
-        type={'password'}
+        type="password"
         placeholder="Пароль"
-        variant={'default'}
-        label={'Пароль'}
+        variant="default"
+        label="Пароль"
+        error={errors.password?.message}
+        {...register('password', {
+          required: 'Введите пароль',
+          minLength: {
+            value: 6,
+            message: 'Пароль минимум 6 символов',
+          },
+        })}
       />
 
-      <Button fullWidth={true} variant={'primary'} size={'sm'}>
+      <Button type="submit" fullWidth variant="primary" size="sm">
         Продолжить
       </Button>
 
@@ -44,7 +100,8 @@ export function RegisterForm() {
 
       <div className={styles.socials}>
         <Button
-          size={'sm'}
+          type="button"
+          size="sm"
           variant="integrations"
           leftIcon={<FaGoogle />}
           fullWidth
@@ -53,7 +110,8 @@ export function RegisterForm() {
         </Button>
 
         <Button
-          size={'sm'}
+          type="button"
+          size="sm"
           variant="integrations"
           leftIcon={<FaGithub />}
           fullWidth
@@ -64,6 +122,7 @@ export function RegisterForm() {
 
       <div className={styles.register}>
         <span>Уже есть аккаунт?</span>
+
         <Link href="/auth/login">Войти</Link>
       </div>
     </form>
